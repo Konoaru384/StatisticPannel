f9f265792147e54643f0910e70487d5a6c34f466
Konoaru <jordan.jeanson31470@gmail.com>
2026-02-11 09:13:15 +0100

    Add files via upload

---
+++ PlayerStats.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StatisticPanel
+{
+    public class PlayerStats
+    {
+        public string Nickname { get; set; } = "Unknown";
+        public int Kills { get; set; }
+        public int Deaths { get; set; }
+        public int TotalPlaySeconds { get; set; }
+        public DateTime LastJoin { get; set; } = DateTime.Now;
+        public DateTime LastSeenUtc { get; set; } = DateTime.UtcNow;
+    }
+}
